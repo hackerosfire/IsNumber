2b64a9d2fee77f7781d91cda58b6366e9889280b
hackerosfire <v6234@abv.bg>
2017-02-13 18:31:03 +0200

    Add project files.

---
+++ IsNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = int.Parse(Console.ReadLine());
+            if (input >= 100 && input <= 200 || input == 0)
+            {
+                Console.WriteLine("");
+            }
+            else
+            {
+                Console.WriteLine("invalid");
+            }
+        }
+    }
+}
